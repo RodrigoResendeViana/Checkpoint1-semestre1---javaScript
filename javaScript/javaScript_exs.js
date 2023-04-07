500896ce77cc22fc6c9943a798a2029e355281da
Rodrigo <digo1209@gmail.com>
2023-04-07 02:05:59 -0300

    adicionando o terceiro exercício

--- javaScript/javaScript_exs.js
+++ javaScript/javaScript_exs.js
@@ -14,7 +14,22 @@ let resultado = numero1 % numero2;
 
 document.write("O resto da divisão de 10 por 3 é ", resultado, "<br><br>");
 
+//exercício 3:
 
+document.write("Exercício 3:<br>")
+
+let altura = prompt('Digite sua altura: ');
+let peso = prompt('Digite seu peso: ');
+
+let imc = peso / (altura * altura);
+
+if(imc < 18.5) {
+    document.write("Seu IMC é ", imc.toFixed(2), "<br>De acordo com seu IMC, você está abaixo do seu peso ideal<br><br>")
+}else if(imc >= 18.5 && imc <= 24.9) {
+    document.write("Seu IMC é ", imc.toFixed(2), "<br>De acordo com seu IMC, você está com o peso ideal<br><br>")
+}else{
+    document.write("Seu IMC é ", imc.toFixed(2), "<br>De acordo com seu IMC, você está acima do seu peso ideal<br><br>")
+}
 
 
 
